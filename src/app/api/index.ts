bd3b765bab31761375e640f2c322d412f5262665
brianshattuck <ubi.eng827@gmail.com>
2024-12-18 05:17:43 -0800

    update API integration

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -2,7 +2,6 @@ import { ProjectStatus, Project } from "state/type";
 import { previousProjects } from "state/projects_temp";
 import { upcomingProjects } from "state/upcoming_projects_temp";
 import { liveProjects } from "state/live_projects_temp";
-import { contentTemp } from "state/content_temp";
 import { convertDateTime, getReminderTimeStampString, getReminderDate } from "../../utils/time";
 
 const BASE_URL = "https://quicklaunchpad.io/";
@@ -19,34 +18,30 @@ export const projectApi = axios.create({
 });
 
 export const contentApi = axios.create({
-  baseURL: BASE_URL,
+  baseURL: CONTENT_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export const getProjectsByStatus = async (status: ProjectStatus) => {
-  // const res = await projectApi.get(`/projects?status=${status}`);
-  // console.log(res);
-  // return res;
-  let projects: any[]  = [];
-  if (status === ProjectStatus.Completed) {
-    projects = previousProjects;
-  } else if (status === ProjectStatus.Live) {
-    projects = liveProjects;
-  } else {
-    projects = upcomingProjects;
+  try {
+    const res = await projectApi.get(`/projects?status=${status}`);
+    const projects = res?.data as any[];
+    return projects.map((p) => {
+      p.pledgeStartDate = convertDateTime(p.pledgeStartDate);
+      p.pledgeEndDate = convertDateTime(p.pledgeEndDate);
+      p.contributionStartDate = convertDateTime(p.contributionStartDate);
+      p.contributionEndDate = convertDateTime(p.contributionEndDate);
+      p.reminderLaunchTime = getReminderTimeStampString(p.pledgeEndDate);
+      p.reminderLaunchTimeBig = getReminderTimeStampString(p.pledgeEndDate ,true);
+      p.reminderDay = getReminderDate(p.pledgeStartDate);
+      return p;
+    });
+  } catch(e) {
+    console.error(e)
+    return [];
   }
-  return projects.map((p) => {
-    p.pledgeStartDate = convertDateTime(p.pledgeStartDate);
-    p.pledgeEndDate = convertDateTime(p.pledgeEndDate);
-    p.contributionStartDate = convertDateTime(p.contributionStartDate);
-    p.contributionEndDate = convertDateTime(p.contributionEndDate);
-    p.reminderLaunchTime = getReminderTimeStampString(p.pledgeEndDate);
-    p.reminderLaunchTimeBig = getReminderTimeStampString(p.pledgeEndDate ,true);
-    p.reminderDay = getReminderDate(p.pledgeStartDate);
-    return p;
-  });  
 };
 
 export const getUpcomingProject= async () => {
@@ -81,8 +76,10 @@ export const getProjectsById = async (pid: string, status: string) => {
 };
 
 export const getProjectsContent = async (contentId: string) => {
-  // const res = await contentApi.get(`${contentId}?populate=*`);
-  // console.log(res);
-  const res = contentTemp;
-  return res;
+  try {
+    const res = await contentApi.get(`${contentId}?populate=*`);
+    return res.data;  
+  } catch (e) {
+    console.error(e)
+  }
 };
