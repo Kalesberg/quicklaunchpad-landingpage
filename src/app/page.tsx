156ac9c56e59a6b9e940a36315eb626d9f2a5cc7
brianshattuck <ubi.eng827@gmail.com>
2024-12-18 07:23:41 -0800

    v1 landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   // 1: The only launch
   // 2: multiple live launches
   // 3: multiple live & previous launches
-  const caseLaunch: number = 3;
+  const caseLaunch: number = 0;
   const [openModal, setOpenModal] = useState(false);
   return (
     <div className="container mx-auto">
