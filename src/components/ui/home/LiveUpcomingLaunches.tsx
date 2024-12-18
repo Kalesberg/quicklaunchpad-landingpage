9c14d16416b61316134e02f96b19f708de60ee85
brianshattuck <ubi.eng827@gmail.com>
2024-12-17 19:44:54 -0800

    integrate backend data on live upcoming card

--- src/components/ui/home/LiveUpcomingLaunches.tsx
+++ src/components/ui/home/LiveUpcomingLaunches.tsx
@@ -1,38 +1,20 @@
 "use client";
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Image from "next/image";
 import Button from "components/common/Button";
 import { useRouter } from "next/navigation";
 
 import { getProjectsByStatus } from "app/api";
-import { ProjectStatus } from "state/type";
+import { Project, ProjectStatus, ChainIdToName } from "state/type";
 
-interface LaunchProps {
-  image: string;
-  name: string;
-  description: string;
-  totalRaise: string;
-  initialPrice: string;
-  launchDate: string;
-  status: "live" | "upcoming" | "tba";
-}
-
-const LaunchCard: React.FC<LaunchProps> = ({
-  image,
-  name,
-  description,
-  totalRaise,
-  initialPrice,
-  launchDate,
-  status,
-}) => {
+const LaunchCard: React.FC<Project> = (p: Project) => {
   const router = useRouter();
   return (
     <div className="bg-[#1B1E29] rounded-2xl overflow-hidden">
       <div className="relative h-48 py-4">
         <Image
-          src={image}
-          alt={name}
+          src="/assets/images/launch-image.png"
+          alt={p.projectName}
           fill
           style={{ objectFit: "cover" }}
           className="rounded-t-2xl"
@@ -59,12 +41,12 @@ const LaunchCard: React.FC<LaunchProps> = ({
           <div className="flex items-center mb-2">
             <Image
               src="/assets/images/project-logo.png"
-              alt={name}
+              alt={p.projectName}
               width={24}
               height={24}
               className="rounded-full mr-2"
             />
-            <h3 className="text-xl font-semibold">{name}</h3>
+            <h3 className="text-xl font-semibold">{p.projectName}</h3>
           </div>
           <Image
             src="/assets/images/chain-avatar.png"
@@ -74,20 +56,20 @@ const LaunchCard: React.FC<LaunchProps> = ({
           />
         </div>
 
-        <p className="text-sm text-[#EBECF2] mb-4">{description}</p>
+        <p className="text-sm text-[#EBECF2] mb-4">{p.description}</p>
 
         <div className="grid grid-cols-1 gap-2 text-sm mb-4">
           <div className="w-full flex justify-between items-center gap-3">
             <p className="text-[#C7CAD9]">Total raise</p>
-            <p>{totalRaise}</p>
+            <p>{p.totalPoolAmount}</p>
           </div>
           <div className="w-full flex justify-between items-center gap-3">
             <p className="text-[#C7CAD9]">Initial price</p>
-            <p>{initialPrice}</p>
+            <p>{p.initialPrice}</p>
           </div>
           <div className="w-full flex justify-between items-center gap-3">
             <p className="text-[#C7CAD9]">Launch date</p>
-            <p>{launchDate}</p>
+            <p>{p.pledgeStartDate}</p>
           </div>
         </div>
         <div className="flex-grow"></div>
@@ -107,10 +89,11 @@ const LaunchCard: React.FC<LaunchProps> = ({
 };
 
 const LiveUpcomingLaunches: React.FC = () => {
+  const [launches, setLaunches] = useState<Project[]>([]);
   const fetchLaunches = useCallback(async () => {
     try {
-      const response = await getProjectsByStatus(ProjectStatus.Live);
-      console.log("live response", response);
+      const projects = await getProjectsByStatus(ProjectStatus.Live);
+      setLaunches(projects)
     } catch (err) {
       console.log("[LiveUpcomingLaunches] projects Club error: ", err);
     }
@@ -120,37 +103,37 @@ const LiveUpcomingLaunches: React.FC = () => {
     fetchLaunches();
   }, [fetchLaunches]);
 
-  const launches: LaunchProps[] = [
-    {
-      image: "/assets/images/launch-image.png",
-      name: "Launch Name",
-      description:
-        "The Most Specialized Blockchain Network for AI Data Monetization & GPU Training",
-      totalRaise: "$150,000",
-      initialPrice: "$0.10",
-      launchDate: "13 Mar 2024",
-      status: "live",
-    },
-    {
-      image: "/assets/images/launch-image-1.png",
-      name: "Launch Name",
-      description:
-        "The Most Specialized Blockchain Network for AI Data Monetization & GPU Training",
-      totalRaise: "$150,000",
-      initialPrice: "$0.10",
-      launchDate: "13 Mar 2024",
-      status: "upcoming",
-    },
-    {
-      image: "/assets/images/launch-image-2.png",
-      name: "Launch Name",
-      description: "Web3 skill-based gaming project",
-      totalRaise: "TBA",
-      initialPrice: "TBA",
-      launchDate: "TBA",
-      status: "tba",
-    },
-  ];
+  // const launches: LaunchProps[] = [
+  //   {
+  //     image: "/assets/images/launch-image.png",
+  //     name: "Launch Name",
+  //     description:
+  //       "The Most Specialized Blockchain Network for AI Data Monetization & GPU Training",
+  //     totalRaise: "$150,000",
+  //     initialPrice: "$0.10",
+  //     launchDate: "13 Mar 2024",
+  //     status: "live",
+  //   },
+  //   {
+  //     image: "/assets/images/launch-image-1.png",
+  //     name: "Launch Name",
+  //     description:
+  //       "The Most Specialized Blockchain Network for AI Data Monetization & GPU Training",
+  //     totalRaise: "$150,000",
+  //     initialPrice: "$0.10",
+  //     launchDate: "13 Mar 2024",
+  //     status: "upcoming",
+  //   },
+  //   {
+  //     image: "/assets/images/launch-image-2.png",
+  //     name: "Launch Name",
+  //     description: "Web3 skill-based gaming project",
+  //     totalRaise: "TBA",
+  //     initialPrice: "TBA",
+  //     launchDate: "TBA",
+  //     status: "tba",
+  //   },
+  // ];
 
   return (
     <section className="mb-12">
