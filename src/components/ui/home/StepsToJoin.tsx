161f04ae3359cc4a09084e36396a1aa172517401
brianshattuck <ubi.eng827@gmail.com>
2024-12-27 04:04:32 -0800

    rename get started button

--- src/components/ui/home/StepsToJoin.tsx
+++ src/components/ui/home/StepsToJoin.tsx
@@ -186,7 +186,7 @@ const StepsToJoin: React.FC<{
         className="min-w-[250px] !bg-[#448AFF] !h-12 mx-auto mt-8 lg:mt-6"
         onClick={handleModal}
       >
-        Get started now
+        {caseLaunch !== 0 ? " Get started now" : "I’m Interested - Keep Me Updated"}        
       </Button>
     </section>
   );
