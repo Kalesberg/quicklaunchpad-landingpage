b922cbbec8386325297d72df737828f139fc071a
brianshattuck <ubi.eng827@gmail.com>
2025-01-21 05:07:14 -0800

    redux for upcoming project

--- src/components/ui/home/LaunchNotice.tsx
+++ src/components/ui/home/LaunchNotice.tsx
@@ -12,15 +12,20 @@ import {
 import { useRouter } from "next/navigation";
 import { getUpcomingProject } from "app/api";
 import { Project } from "state/type";
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { updateUpcomingProject } from "../../../reduxStore/rootReducer";
 
 const LaunchNotice: React.FC<{ status?: string }> = ({ status }) => {
+  const dispatch = useDispatch();
   const router = useRouter();
+  const { upcomingProject } = useSelector((state: { upcomingProject: Project }) => state || {});
 
-  const [project, setProject] = useState<Project | null>(null);
+  // const [project, setProject] = useState<Project | null>(null);
   const fetchLaunches = useCallback(async () => {
     try {
       const p = await getUpcomingProject();
-      setProject(p)
+      dispatch(updateUpcomingProject(p));
     } catch (err) {
       console.log("[LiveUpcomingLaunches] projects Club error: ", err);
     }
@@ -30,7 +35,7 @@ const LaunchNotice: React.FC<{ status?: string }> = ({ status }) => {
   }, [fetchLaunches]);
 
   return (
-    project && <div className="rounded-2xl p-8 mt-[30px] mb-[25px] relative overflow-hidden">
+    upcomingProject && <div className="rounded-2xl p-8 mt-[30px] mb-[25px] relative overflow-hidden">
       <div className="relative z-10">
         <div className="relative flex items-center flex-wrap justify-start sm:justify-between md:flex-nowrap gap-1 md:gap-0">
           <div className="flex items-center mb-2">
@@ -41,21 +46,21 @@ const LaunchNotice: React.FC<{ status?: string }> = ({ status }) => {
               height={32}
               className="rounded-full mr-2"
             />
-            <h2 className="text-xl font-bold">{project.projectName}</h2>
+            <h2 className="text-xl font-bold">{upcomingProject.projectName}</h2>
           </div>
           <div className="relative flex items-center text-sm z-10">
             <span
               className={`text-black font-bold px-2 py-1 rounded-md text-xs ${
-                project.status === "pledging"
+                upcomingProject.status === "pledging"
                   ? "bg-[#0FC679]"
-                  : project.status === "upcoming"
+                  : upcomingProject.status === "upcoming"
                     ? "bg-[#FDD835]"
                     : "bg-[#C684FF]"
               }`}
             >
-              {project.status === "pledging"
+              {upcomingProject.status === "pledging"
                 ? "Open"
-                : project.status === "upcoming"
+                : upcomingProject.status === "upcoming"
                   ? "Upcoming"
                   : "Closed"}
             </span>
@@ -63,36 +68,36 @@ const LaunchNotice: React.FC<{ status?: string }> = ({ status }) => {
         </div>
 
         <div className="flex space-x-5 mt-2 mb-4">
-          {project.socials?.telegram && (<a href={project.socials.telegram} target="_blank">
+          {upcomingProject.socials?.telegram && (<a href={upcomingProject.socials.telegram} target="_blank">
           <TelegramIcon className="w-4 h-4 text-gray-400 hover:text-white cursor-pointer" />
           </a>)}
-          {project.socials?.youtube && (<a href={project.socials.youtube} target="_blank">
+          {upcomingProject.socials?.youtube && (<a href={upcomingProject.socials.youtube} target="_blank">
             <YoutubeIcon className="w-4 h-4 text-gray-400 hover:text-white cursor-pointer" />
           </a>)}
-          {project.socials?.discord && (<a href={project.socials.discord} target="_blank">
+          {upcomingProject.socials?.discord && (<a href={upcomingProject.socials.discord} target="_blank">
             <DiscordIcon className="w-4 h-4 text-gray-400 hover:text-white cursor-pointer" />
           </a>)}
-          {project.socials?.github && (<a href={project.socials.github} target="_blank">
+          {upcomingProject.socials?.github && (<a href={upcomingProject.socials.github} target="_blank">
             <GithubIcon className="w-4 h-4 text-gray-400 hover:text-white cursor-pointer" />
           </a>)}
-          {project.socials?.twitter && (<a href={project.socials.twitter} target="_blank">
+          {upcomingProject.socials?.twitter && (<a href={upcomingProject.socials.twitter} target="_blank">
             <TwitterIcon className="w-4 h-4 text-gray-400 hover:text-white cursor-pointer" />
           </a>)}
         </div>
         <p className="text-xl font-bold text-white mb-2">
-          {project.projectName} is now live on QuickSwap Launchpad
+          {upcomingProject.projectName} is now live on QuickSwap Launchpad
         </p>
         <p className="text-sm text-[#EBECF2] mb-4">
-          {project.description}
+          {upcomingProject.description}
         </p>
         <div className="flex flex-col gap-2 justify-start text-[#EBECF2] items-start text-sm mb-4">
-          <p>Total raise: ${project.totalPoolAmount}</p>
-          <p>Initial price: ${project.initialPrice}</p>
-          <p>Launch date: {project.pledgeStartDate}</p>
+          <p>Total raise: ${upcomingProject.totalPoolAmount}</p>
+          <p>Initial price: ${upcomingProject.initialPrice}</p>
+          <p>Launch date: {upcomingProject.pledgeStartDate}</p>
         </div>
         <Button
           onClick={() =>
-            router.push(`/dashboard/launch-info/${project.pid}?status=${project.status}`)
+            router.push(`/dashboard/launch-info/${upcomingProject.pid}?status=${upcomingProject.status}`)
           }
           variant="secondary"
           size="medium"
