481d8e435fd4afeb19082751956b7afdead719d5
brianshattuck <ubi.eng827@gmail.com>
2025-01-22 10:19:00 -0800

    add a function to close columns modal

--- src/components/common/ColumnsModal.tsx
+++ src/components/common/ColumnsModal.tsx
@@ -20,8 +20,8 @@ const ColumnsModal: React.FC<{
     }
   
     return (
-      openModal && (
-        <div className="absolute top-[60px] left-0 w-[220px] bg-[#1b1e29] shadow-sm shadow-slate-800 p-4 rounded-2xl z-[999]">
+      openModal && (<>
+              <div className="absolute top-[60px] left-0 w-[220px] bg-[#1b1e29] shadow-sm shadow-slate-800 p-4 rounded-2xl z-[999]">
           {Object.keys(columns).map((key) =>
           <div className="mt-3">
             <Checkbox
@@ -33,6 +33,8 @@ const ColumnsModal: React.FC<{
           </div>
           )}
         </div>
+        <div className="fixed w-full h-full top-0 left-0 z-[99] " onClick={() => setOpenModal(false)}></div>
+      </>
       )
     );
   };
